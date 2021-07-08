cfa94aca3b5481d213a37140db4f5288321eed4b
Joe Bragel <Bragel@Joes-MBP.attlocal.net>
2021-07-08 16:13:10 -0500

    indie game app save 5

--- app.js
+++ app.js
@@ -9,13 +9,36 @@ $.ajax({
 }).then((data) => {
 
   const openModal = () => {
+    $(".title").empty()
+    $(".genres").empty()
+    $(".screenshots").empty()
+    $(".platforms").empty()
     $("#modal").css("display", "block")
     const index = $(event.currentTarget).attr("index")
     $("<h1>").text(data.results[index].name).appendTo(".title")
+    //$(".genres").text("Genres: " + data.results[index].genres)
     $(".rating").text("Average Rating: " + data.results[index].metacritic)
     $(".release").text("Release Date: " + data.results[index].released)
     //had to multiply playtime by 5 because their data for that was WAY under average playtime
     $(".playtime").text("How Long to Beat: " + data.results[index].playtime * 5 + " hours")
+    //genres loop
+    for (let i = 0; i < data.results[index].genres.length; i++) {
+      $(".genres").append(data.results[index].genres[i].name + " ")
+    }
+    $(".genres").prepend("Genres: ")
+    //screenshots
+    $(".screenshots").append($(`<img src="${data.results[index].short_screenshots[1].image}" />`))
+    $(".screenshots").append($(`<img src="${data.results[index].short_screenshots[2].image}" />`))
+    $(".screenshots").append($(`<img src="${data.results[index].short_screenshots[3].image}" />`))
+    //platforms loop
+    for (let i = 0; i < data.results[index].platforms.length; i++) {
+      $(".platforms").append(data.results[index].platforms[i].platform.name + " ")
+    }
+    $(".platforms").prepend("Platforms: ")
+  }
+
+  const closeModal = () => {
+    $("#modal").css("display", "none")
   }
 
   for (let i = 0; i < data.results.length; i++) {
@@ -35,8 +58,9 @@ $.ajax({
     //   console.log(data.results.indexOf(event.currentTarget));
     // })
 
-}
 
+}
+  $("#modal").click(closeModal)
  //$("<h4>").text(data.results[i].name.appendTo("#modalbox"))
 console.log(data);
 
