9ced126b9467d13c5cec128ee6a14cad92a0e3e0
Joe Bragel <Bragel@Joes-MBP.attlocal.net>
2021-07-07 23:00:37 -0500

    indie game project save 2 cards and early modal

--- app.js
+++ app.js
@@ -8,14 +8,25 @@ $.ajax({
 
 }).then((data) => {
 
-for (let i = 0; i < data.results.length; i++) {
-  $("<div>").addClass("card" + i).appendTo(".games")
-  $("<img>").attr("src", data.results[i].background_image)//.addClass("image" + i)
-  .appendTo(".card" + i)
-  $("<h4>").text(data.results[i].name).appendTo(".card" + i)
-  //$("<div>").addClass("title").appendTo(".games")
-  // $("<div>").text("Game Title").addClass("titletext").appendTo("img")
+  const openModal = () => {
+    $("#modal").css("display", "block")
+  }
+
+  for (let i = 0; i < data.results.length; i++) {
+    $("<div>").addClass("card" + i).appendTo(".games")
+    .css( //style for the cards
+    { "background-color": "black",
+      "margin": "2px",
+      "border-radius": "5px",
+    }).click(openModal)
+    $("<img>").attr("src", data.results[i].background_image)//.addClass("image" + i)
+    .appendTo(".card" + i)
+    $("<h4>").text(data.results[i].name).addClass(".cardtext").appendTo(".card" + i)
+    //$("<div>").addClass("title").appendTo(".games")
+    // $("<div>").text("Game Title").addClass("titletext").appendTo("img")
 }
+
+
 
 // $("<img>").attr("src", data.results[currentGame].background_image).appendTo(".games");
 // $(".next").on("click", () => {
